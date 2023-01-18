82fd83aa2b9ba28a5c11548a74853380c9be26b1
Eml-coder <labiche6789@gmail.com>
2023-01-18 14:08:48 -0500

    Designing the Checkout Page part1

--- src/components/CartDropdown/CartDropdownComponent.jsx
+++ src/components/CartDropdown/CartDropdownComponent.jsx
@@ -8,22 +8,25 @@ import Button from '../Button/ButtonComponent';
 import CartItem from '../CartItem/CartItemComponent';
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
-  const navigate = useNavigate();
+	const { cartItems } = useContext(CartContext);
+	const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
-    navigate('/checkout');
-  }
-  return (
-    <div className='cart-dropdown-container'>
-        <div className='cart-items'>
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-          ))}
-            </div>
-<Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
-  )
-}
+	const goToCheckoutHandler = () => {
+		navigate('/checkout');
+	};
+	return (
+		<div className='cart-dropdown-container'>
+			<div className='cart-items'>
+				{cartItems.map((item) => (
+					<CartItem
+						key={item.id}
+						cartItem={item}
+					/>
+				))}
+			</div>
+			<Button onClick={goToCheckoutHandler}>CHECK OUT</Button>
+		</div>
+	);
+};
 
-export default CartDropdown
+export default CartDropdown;
