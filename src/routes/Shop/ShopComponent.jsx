06ef458162d9f818408ce3a9cae417d38f31fec3
Eml-coder <labiche6789@gmail.com>
2023-01-19 14:50:26 -0500

    Added the preview of products to the shop page

--- src/routes/Shop/ShopComponent.jsx
+++ src/routes/Shop/ShopComponent.jsx
@@ -1,28 +1,21 @@
-import { useContext, Fragment } from 'react';
+import { useContext } from 'react';
 
 import { CategoriesContext } from '../../contexts/CategoriesContext';
-import ProductCard from '../../components/ProductCard/ProductCardComponent';
+import CartegoryPreview from '../../components/CategoryPreview/CartegoryPreviewComponent';
 
 import './ShopStyles.scss';
 
 const Shop = () => {
 	const { categoriesMap } = useContext(CategoriesContext);
 	return (
-		<Fragment>
-			{Object.keys(categoriesMap).map((title) => (
-				<Fragment key={title}>
-					<h2>{title}</h2>
-					<div className='products-container'>
-						{categoriesMap[title].map((product) => (
-							<ProductCard
-								key={product.id}
-								product={product}
-							/>
-						))}
-					</div>
-				</Fragment>
-			))}
-		</Fragment>
+		<div className='shop-container'>
+			{Object.keys(categoriesMap).map((title) => {
+				const products = categoriesMap[title];
+				return (
+					<CartegoryPreview key={title} title={title} products={products} />
+				);
+			})}
+		</div>
 	);
 };
 
