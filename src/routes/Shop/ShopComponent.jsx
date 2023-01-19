1ed34e4b4ea9236eff8e6a9f0c28a598b0e041d8
Eml-coder <labiche6789@gmail.com>
2023-01-19 15:24:49 -0500

    More structure to the shop page

--- src/routes/Shop/ShopComponent.jsx
+++ src/routes/Shop/ShopComponent.jsx
@@ -1,21 +1,15 @@
-import { useContext } from 'react';
-
-import { CategoriesContext } from '../../contexts/CategoriesContext';
-import CartegoryPreview from '../../components/CategoryPreview/CartegoryPreviewComponent';
-
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../CategoriesPreview/CategoriesPreviewComponent';
+import Category from '../Category/CategoryComponent';
 import './ShopStyles.scss';
 
 const Shop = () => {
-	const { categoriesMap } = useContext(CategoriesContext);
+
 	return (
-		<div className='shop-container'>
-			{Object.keys(categoriesMap).map((title) => {
-				const products = categoriesMap[title];
-				return (
-					<CartegoryPreview key={title} title={title} products={products} />
-				);
-			})}
-		</div>
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+			<Route path=':category' element={<Category />} />
+		</Routes>
 	);
 };
 
